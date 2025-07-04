d1b2eb9d2ce78b5687239dad965141752ae8c1b9
TerratekMusic <sunset.dfilms@gmail.com>
2025-07-03 19:47:10 -0600

    INIT FUSION

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+using Fusion.Sockets;
+using UnityEngine.SceneManagement;
+
+namespace BEKStudio
+{
+    public class NetworkManager : MonoBehaviour, INetworkRunnerCallbacks
+    {
+        public static NetworkManager Instance;
+
+        [Header("Photon Fusion")]
+        public NetworkRunner runnerPrefab;
+        public NetworkObject playerPrefab;  // Asegúrate de asignar esto en el Inspector
+
+        private NetworkRunner _runnerInstance;
+        public NetworkRunner Runner => _runnerInstance;
+
+        private void Awake()
+        {
+            if (Instance == null)
+                Instance = this;
+            else
+                Destroy(gameObject);
+        }
+
+        private async void Start()
+        {
+            _runnerInstance = Instantiate(runnerPrefab);
+            _runnerInstance.name = "Runner";
+            DontDestroyOnLoad(_runnerInstance);
+            _runnerInstance.ProvideInput = true;
+
+            await _runnerInstance.StartGame(new StartGameArgs
+            {
+                GameMode = GameMode.Shared,
+                SessionName = "Snake2048Room",
+                Scene = SceneRef.FromIndex(SceneManager.GetActiveScene().buildIndex),
+                SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
+            });
+        }
+
+        // public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+        // {
+        //     Debug.Log("Player joined: " + player);
+
+        //     runner.Spawn(playerPrefab, new Vector3(0, 1, 0), Quaternion.identity, player);
+        // }
+
+        public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+{
+    // El spawn se maneja desde PlayerSpawner.cs
+}
+
+        public void OnInput(NetworkRunner runner, NetworkInput input)
+        {
+            Vector2 direction = Vector2.zero;
+
+            if (Input.GetKey(KeyCode.UpArrow)) direction = Vector2.up;
+            if (Input.GetKey(KeyCode.DownArrow)) direction = Vector2.down;
+            if (Input.GetKey(KeyCode.LeftArrow)) direction = Vector2.left;
+            if (Input.GetKey(KeyCode.RightArrow)) direction = Vector2.right;
+
+            input.Set(new SnakeInputData { direction = direction });
+        }
+
+        // Requerido por Fusion (callbacks vacíos o de logging)
+        public void OnConnectedToServer(NetworkRunner runner) { }
+        public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason) => Debug.LogWarning($"Disconnected: {reason}");
+        public void OnPlayerLeft(NetworkRunner runner, PlayerRef player) { }
+        public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
+        public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason) { }
+        public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) { }
+        public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) { }
+        public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
+        public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) { }
+        public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) { }
+        public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) { }
+        public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, System.ArraySegment<byte> data) { }
+        public void OnSceneLoadDone(NetworkRunner runner) { }
+        public void OnSceneLoadStart(NetworkRunner runner) { }
+        public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress) { }
+        public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+        public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+    }
+}
+
