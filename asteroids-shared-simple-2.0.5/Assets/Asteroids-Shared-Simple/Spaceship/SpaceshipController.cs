d97e99e3eb01d04947591e2eaf4194ec7369f862
TerratekMusic <sunset.dfilms@gmail.com>
2025-08-11 14:12:30 -0600

    Bullet Improve and Player 2 color change

--- asteroids-shared-simple-2.0.5/Assets/Asteroids-Shared-Simple/Spaceship/SpaceshipController.cs
+++ asteroids-shared-simple-2.0.5/Assets/Asteroids-Shared-Simple/Spaceship/SpaceshipController.cs
@@ -114,7 +114,7 @@ namespace Asteroids.SharedSimple
             {
                 case 0: return Color.red;
                 case 1: return Color.green;
-                case 2: return Color.blue;
+                case 2: return Color.cyan;
                 case 3: return Color.yellow;
                 case 4: return Color.cyan;
                 case 5: return Color.grey;
