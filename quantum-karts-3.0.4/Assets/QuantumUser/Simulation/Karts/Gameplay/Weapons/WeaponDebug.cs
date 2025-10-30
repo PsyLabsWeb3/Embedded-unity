8ff088795984eccc046c21199a6cdc4f2f0c0b50
Rafael <rafa.acuna.96@gmail.com>
2025-10-30 03:48:08 -0600

    Cypherpunk Hackathon Submission

---
+++ quantum-karts-3.0.4/Assets/QuantumUser/Simulation/Karts/Gameplay/Weapons/WeaponDebug.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Photon.Deterministic;
+using UnityEngine;
+
+namespace Quantum
+{
+    [Serializable]
+    unsafe partial class WeaponDebug : WeaponAsset
+    {
+        public override void Activate(Frame f, EntityRef sourceKartEntity)
+        {
+            Debug.Log("Activate debug weapon");
+        }
+
+        public override bool AIShouldUse(Frame f, EntityRef aiKartEntity)
+        {
+            return true;
+        }
+    }
+}
---
+++ quantum-karts-3.0.4/Assets/QuantumUser/Simulation/Karts/Gameplay/Weapons/WeaponPickup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Photon.Deterministic;
+
+namespace Quantum
+{
+    [Serializable]
+    public unsafe partial class WeaponPickup : PickupAsset
+    {
+        public AssetRef<WeaponAsset> PredeterminedWeapon;
+
+        public override void OnPickup(Frame f, EntityRef kartEntity)
+        {
+            if (f.Unsafe.TryGetPointer(kartEntity, out KartWeapons* kartWeapons))
+            {
+                kartWeapons->GiveWeapon(f, kartEntity, GetWeaponAsset(f));
+            }
+        }
+
+        public AssetRef<WeaponAsset> GetWeaponAsset(Frame f)
+        {
+            RaceSettings rs = f.FindAsset(f.RuntimeConfig.RaceSettings);
+            return PredeterminedWeapon != null ? PredeterminedWeapon : rs.GetRandomWeapon(f);
+        }
+
+        public override FP GetRespawnTime(Frame f)
+        {
+            RaceSettings rs = f.FindAsset(f.RuntimeConfig.RaceSettings);
+            return rs.PickupRespawnTime;
+        }
+    }
+}
